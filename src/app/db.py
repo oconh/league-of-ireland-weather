c0ad7ed9039997e0ebd2cf63eda1e1c19d79d359
oconh <oconnellharry1@gmail.com>
2026-01-30 17:30:52 +0000

    Database credentials to env variables

--- src/app/db.py
+++ src/app/db.py
@@ -1,13 +1,14 @@
+import os
 import psycopg2
 import pandas as pd
 
 def get_conn():
     return psycopg2.connect(
-        host="localhost",
-        dbname="weather",
-        user="weather_user",
-        password="weather_pass",
-        port=5432
+        host=os.getenv("DB_HOST", "localhost"),
+        dbname=os.getenv("DB_NAME", "weather"),
+        user=os.getenv("DB_USER", "weather_user"),
+        password=os.getenv("DB_PASS"),
+        port=int(os.getenv("DB_PORT", 5432))
     )
 
 def fetch_df(query, params=None):
